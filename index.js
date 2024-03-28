b7b6b52359788790730cd0c7aa7d85900d9b0175
Anandha meenachi M <157224732+anandhameenachi@users.noreply.github.com>
2024-03-28 21:13:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function handleNavigation(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block'; 
+        } else {
+            section.style.display = 'none'; 
+        }
+    });
+}
